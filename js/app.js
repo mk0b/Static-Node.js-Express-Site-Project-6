cfabcf285bcb62a40d710977ec1fc22f0b4ea541
Megan Katherine O'Brien <megankob@gmail.com>
2019-12-13 10:19:42 -0500

    Start Project Files and some notes.

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+
+
+//TODO: Create a gitignore file and make sure that node_modules is in it so the repo doesn't store or track taht folder.
+//TODO: run npm init in my folder
+//TODO: Install express and pug (maybe others?) and save as dependancies
+/*TODO: Take/Add the following to imgs folder:
+    - A professional profile pic | 550px by 350px
+    - Project Screenshots at least two each:
+        - A main shot for the landing page | Square 550px by 550px
+        - Between one and three images that can be any dimensions but work
+        well in this project as landscape images | 1200px by 550px
+*/
+/*TODO: Add project data:
+    - Create a data.json file at the root directory.
+    - Create an object literal that contains a single property called projects.
+    - Projects will be an array containing an object for reach project in portfolio.
+    - Each project object should contain the following:
+        - unique id name id 
+        - project_name
+        - description
+        - technologies - an array of strings containing a list of the technologies used for that project
+        - live_link - link to the LIVE version of the project - host with Github pages.
+        - github_link
+        - image_urls - an array of strings containing file paths (from the views folder to the images themselves?)
+        You'll need a main image to be shown on the landing page, and three images to be shown on project page.
+*/
+
+//Continue the instructions
